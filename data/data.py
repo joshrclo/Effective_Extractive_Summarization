23432b4fbad38f35f813e68652a5a4c429ed66d8
Joshua Lo <33582835+joshrclo@users.noreply.github.com>
2021-05-19 11:15:55 -0400

    Update data.py

--- data/data.py
+++ data/data.py
@@ -6,6 +6,18 @@ from os.path import join
 
 from torch.utils.data import Dataset
 
+class ImgDmDataset(Dataset):
+    def __init__(self, path: str) -> None:
+        self._data_path = path
+        self._names, self._n_data = list_data(self._data_path)
+
+    def __len__(self) -> int:
+        return self._n_data
+
+    def __getitem__(self, i: int):
+        with open(join(self._data_path, self._names[i])) as f:
+            js = json.loads(f.read())
+        return js
 
 class CnnDmDataset(Dataset):
     def __init__(self, split: str, path: str) -> None:
